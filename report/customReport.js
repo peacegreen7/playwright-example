16ecc2a857b52414abd5a40e2640199c060b5eb1
yenhtt <yenhtt@vng.com.vn>
2023-09-04 15:00:26 +0700

    edit report config

--- report/customReport.js
+++ report/customReport.js
@@ -2,12 +2,11 @@
 
 //** @implements {import('@playwright/test/reporter').Reporter} */
 class MyReporter {
-  constructor(options) {
-    console.log(`my-awesome-reporter setup with customOption set to ${options.customOption}`);
-  }
 
   onBegin(config, suite) {
+    // send the data into kibana/grafana/3rd party in-house / other services
     console.log(`Starting the run with ${suite.allTests().length} tests`);
+    console.log(`Send log to grafana`);
   }
 
   onTestBegin(test) {
